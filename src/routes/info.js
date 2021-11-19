7d9f51d126b5632585a1eba818632ad80b1030de
EstefanyAguirre12 <estefanyaguirre1200@hotmail.com>
2021-11-19 17:41:22 -0600

    Update and Delete info

--- src/routes/info.js
+++ src/routes/info.js
@@ -21,4 +21,24 @@ router.get("/infos", (req, res) => {
     .catch((error) => res.json({ message: error }));
 });
 
+// delete a user
+router.delete("/infos/:id", (req, res) => {
+  const { id } = req.params;
+  infoSchema
+    .remove({ _id: id })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+// update a user
+router.put("/infos/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  infoSchema
+    .updateOne({ _id: id }, { $set: { name } })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+
 module.exports = router;

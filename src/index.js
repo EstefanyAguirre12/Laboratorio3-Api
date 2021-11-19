4529a88745b5069a1b74fe956a6b935f46feff61
EstefanyAguirre12 <estefanyaguirre1200@hotmail.com>
2021-11-19 14:44:54 -0600

    Connect to Database

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 
 const app = express();
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 3000;
 
 // routes
 app.get("/", (req, res) => {
     res.send("Welcome to my API");
 });
+
+// mongodb connection
+// connect to mongodb
+mongoose.connect('mongodb://suUsuario:suContraseña@localhost:27017/dbLabo?authSource=admin', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(()=>console.log("Conect"));
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongodb connection error:'));
 
 app.listen(port, () => console.log("Server listening to", port));

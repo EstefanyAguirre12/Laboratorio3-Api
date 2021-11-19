4112bf8f70067afa36108179d987cd7e8b07b84e
EstefanyAguirre12 <estefanyaguirre1200@hotmail.com>
2021-11-19 14:27:10 -0600

    Create Api

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+// routes
+app.get("/", (req, res) => {
+    res.send("Welcome to my API");
+});
+
+app.listen(port, () => console.log("Server listening to", port));
